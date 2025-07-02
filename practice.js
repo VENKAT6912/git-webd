e8ac7659d8c3d07032d93f333fa68c5fb65819bc
VENKAT6912 <vibecoder18@gmail.com>
2025-07-02 23:51:37 +0530

    web pages(basic)

---
+++ practice.js
@@ -0,0 +1,51 @@
+// console.log("Hi!");
+
+// function timeout() {
+//     console.log("Click the button!");
+// }
+
+// setTimeout(timeout, 15000);
+
+// console.log("Welcome to loupe.");
+
+// NOTES:::
+// const fs = require('fs');
+
+// console.log("1. Start reading file...");
+
+// // Async read file with callback
+
+// function source(err,data){
+//     console.log("2.Found nothing");
+//     setTimeout(() => {
+//     console.log("3. Processing file content after 2 seconds...");
+//     console.log("4. File content:", data);
+//   }, 2000);
+// }
+// fs.readFile('a.txt', 'utf-8', source);
+
+// console.log("5. Code continues while file is being read...");
+// run in loupe and vs code and illustrate what is happening u can remind all 2 hr concept in this single code
+
+// class rect{
+//     constructor(wid,hgh,clr){
+//         this.wid=wid;
+//         this.hgh=hgh;
+//         this.clr=clr;
+//     }
+//     area(){
+//         return this.wid*this.hgh;
+//     }
+//     color(){
+//         console.log(`this color is ${this.clr}`)
+//     }
+// }
+// const rect1=new rect(2,4,"red")
+// const area=rect1.area()
+// console.log(area)
+// rect1.color()
+//  const map=new Map();
+//  map.set('name','hello')
+//  map.set('no',30)
+
+//  console.log(map.get('name'))
